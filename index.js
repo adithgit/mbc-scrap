f2c961b3bed5089455d0e79b7d76eac064abb311
adithgit <5285@mbcpeermade.com>
2022-06-11 00:03:14 +0530

    fetching details implemented

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
-// const request = require('request-promise').defaults({ jar: true});
-// const cheerio = require('cheerio');
+const request = require('request-promise').defaults({ jar: true});
+const cheerio = require('cheerio');
 
 
 
-// async function main(){
-//     const result =  await request.get("http://mbccet.com/login.php");
-//     const loginResult = await request.post("http://mbccet.com/get_acess.php",{
-//         form:{
-//             rno: "5285",
-//             rq_type:"use_rno"
-//         }
-//     })
-//     const studDetails = await request.get("http://mbccet.com/student_details.php");
+async function getAttendance( admissionNum ){
+    const result =  await request.get("http://mbccet.com/login.php");
+    const loginResult = await request.post("http://mbccet.com/get_acess.php",{
+        form:{
+            rno: admissionNum,
+            rq_type:"use_rno"
+        }
+    })
+    const studDetails = await request.get("http://mbccet.com/student_details.php");
     
 
-//     const $ = cheerio.load( studDetails );
+    const $ = cheerio.load( studDetails );
 
-//     const titles = $('table .klub_title').text().split(/\s\s+ /);
-//     titles.pop(); titles.shift();
-//     const data = $('table .klub_data').text().split(/\s\s+/g); 
-//     data.shift(); data.pop(); 
-//     let resultData = "";
+    const titles = $('table .klub_title').text().split(/\s\s+ /);
+    titles.pop(); titles.shift();
+    const data = $('table .klub_data').text().split(/\s\s+/g); 
+    data.shift(); data.pop(); 
+    let resultData = "";
 
-//     for (let index = 0; index < 6; index++) {
-//         resultData += (titles[index]+" -- "+data[index]) +"\n";        
-//     }
+    for (let index = 0; index < 6; index++) {
+        resultData += (titles[index]+" -- "+data[index]) +"\n";        
+    }
 
 
-//     let subjectNodes = $("table tbody")[2].childNodes
-//     let count = -1;
+    let subjectNodes = $("table tbody")[2].childNodes
+    let count = -1;
     
-//     for( index in subjectNodes ){
-//         if( subjectNodes[ index ].nodeType != 1) continue;
-//         count++;
-//     }
+    for( index in subjectNodes ){
+        if( subjectNodes[ index ].nodeType != 1) continue;
+        count++;
+    }
 
     
-//     data.splice(0,6);
-//     data.splice(count*7);
-//     while( data.length ){
-//         let subData = data.splice(0,7);
-//         resultData += subData.join(" , ")+"\n";
-//     }
-//     console.log(resultData);
-// }
+    data.splice(0,6);
+    data.splice(count*7);
+    while( data.length ){
+        let subData = data.splice(0,7);
+        resultData += subData.join(" , ")+"\n";
+    }
+
+    return new Promise((resolve, reject)=>{
+        if( resultData ) resolve( resultData );
+        reject( false );
+    })
+}
 
 
-// main();
 
 
 
@@ -65,12 +68,19 @@ client.once("ready", ()=>{
 })
 
 client.on("messageCreate", message =>{
-    if( message.content.startsWith(">")){
-        if(message.content.substring(1)==='ping'){
-            message.reply("Pong!!!")
+    if( message.content.startsWith("#")){
+        if(message.content.substring(1,4)==='get'){
+           getAttendance( message.content.substring(4).trim() ).then(( result )=>{
+               message.reply( result );
+           }).catch(( err )=>{
+            message.reply(" Please check your query again or try later.")
+           });
+        }else{
+            message.reply("Invalid request!!")
         }
     }
 })
+
 
 
 client.login(
